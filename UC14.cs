ec1cc064b219f4b82b85ed97c51010e9f9e458ec
Raghav <raghavwandre1234@gmail.com>
2021-09-28 21:47:49 +0530

    UC14

---
+++ UC14.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day10
+{
+    interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+}
